d996dee0d94413aa5565eefef2f04a53bc70ce8e
Ray Wojciechowski <ray@skillbird.de>
2021-08-01 14:56:38 +0200

    Implement example

--- AnotherTestApp/ContentView.swift
+++ AnotherTestApp/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var presentingSheet = false
+  @State private var tmpEvent = Event(desc: "default Event")
+  @State private var counter = 0
+  
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+        Text("Hello Test App")
+      }
+      .toolbar {
+        Button(action: showSheet) {
+          Label("Open sheet", systemImage: "plus")
+        }
+      }
+      .sheet(isPresented: $presentingSheet) {
+        NavigationView {
+          VStack {
+            Text("event: \(tmpEvent.desc)")
+          }
+          .navigationBarTitleDisplayMode(.inline)
+          .navigationTitle(tmpEvent.desc)
+          .toolbar {
+            Button(action: { presentingSheet = false }) {
+              Label("Close", systemImage: "xmark")
+            }
+          }
+        }
+      }
     }
+  }
+  
+  private func showSheet() {
+    counter += 1
+    tmpEvent = Event(desc: "new Event \(counter)")
+    presentingSheet = true
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ AnotherTestApp/Event.swift
@@ -0,0 +1,12 @@
+//
+//  Event.swift
+//  AnotherTestApp
+//
+//  Created by Ray Wojciechowski on 01.08.21.
+//
+
+import Foundation
+
+struct Event {
+  var desc: String
+}
