2a8c06d4a1fddc710ce5cfaf3ea65af198cc8ebf
Ray Wojciechowski <ray@skillbird.de>
2021-08-01 13:45:49 +0200

    Initial Commit

---
+++ AnotherTestApp/AnotherTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherTestAppApp.swift
+//  AnotherTestApp
+//
+//  Created by Ray Wojciechowski on 01.08.21.
+//
+
+import SwiftUI
+
+@main
+struct AnotherTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnotherTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnotherTestApp
+//
+//  Created by Ray Wojciechowski on 01.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
